3b644c1d0121c8b593b4e43ddd455c868f4fef07
Eduardo Teixeira <dunirvana@gmail.com>
2022-04-18 14:19:08 -0300

    Reorganize authentication forms.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "posts#index"
+
+  devise_for :users, only: %i[sessions registrations passwords]
 
   resources :posts, only: %i[ index show new create ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
