dd53868207572fd6176463811ca9703ceaa21ef6
Eduardo Teixeira <dunirvana@gmail.com>
2022-04-29 22:34:37 -0300

    Included comments with rich text.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,51 +1,44 @@
 class PostsController < ApplicationController
   include SuggestedUsers
 
+  before_action :set_post, only: %i[show]
   before_action :set_suggested_users, only: %i[index]
 
-  before_action :set_post, only: %i[ show  ]
-
-  # GET /posts or /posts.json
   def index
-    # flash.now[:notice] = "Notice!"
+    # flash.now[:notice] = "Deu certo!"
+    # flash.now[:alert] = "Boooo"
 
     @posts = Post.all.order(created_at: :desc)
   end
 
-  # GET /posts/1 or /posts/1.json
   def show
+    @comment = Comment.new
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # POST /posts or /posts.json
   def create
     @post = Post.new(post_params.merge(created_by: current_user))
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post foi criado com sucesso." }
-        format.json { render :show, status: :created, location: @post }
-      else
-        flash.now[:alert] = @post.errors.full_messages.to_sentence
+    if @post.save
+      PostChannel.broadcast_to "post_channel", post_created: render_to_string(partial: @post)
 
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+      redirect_to @post, notice: 'Post foi criado com sucesso.'
+    else
+      flash.now[:alert] = @post.errors.full_messages.to_sentence
+      render :new
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:photo, :description)
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:photo, :description)
+  end
 end
